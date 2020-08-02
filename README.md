770122c8aed176d0e5a7d06aafe337f29483ed66
rghosh8 <rajat.ghosh11@gmail.com>
2020-08-01 23:52:56 -0700

    added

---
+++ README.md
@@ -0,0 +1,6 @@
+This is my first pip file. the intent of this pip file is to include all the utilities libraries to plot in python
+
+```Python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eda_utils", # Replace with your own username
+    version="0.0.1",
+    author="rajat ghosh",
+    author_email="rajat.ghosh11@gmail.com",
+    description="this is an experimental package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rghosh8/firstpip.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
